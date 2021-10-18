007e1a452f30c628e4027001e0cfad7772b7c470
hossyn assghary <hossynassghary@gmail.com>
2021-10-19 00:08:47 +0330

    change index test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Button} from "@mui/material";
 
 function App() {
 
   return (
     <div className="App">
+        <Button>hi</Button>
       <h1>hi</h1>
       </div>
 
