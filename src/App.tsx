0a9eecc87740d9c0593c2da15e4d95170d64ff15
hossyn assghary <hossynassghary@gmail.com>
2021-10-18 23:47:00 +0330

    change index

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {AppBar, Box, Button, Drawer, IconButton, List, ListItemButton, ListItemIcon, Toolbar} from "@mui/material";
+import {GridMenuIcon} from "@mui/x-data-grid";
+import {DataUsageTwoTone, RssFeedOutlined, Sync} from "@mui/icons-material";
 
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Drawer onClick={()=>{setDrawerOpen(!drawerOpen)}} open={drawerOpen} id="Drawer" anchor="left">
+          <Box sx={{width: 200}}>
+            <List>
+              <ListItemButton sx={{width: '100%', height: 40}}>
+                <ListItemIcon>
+                  <RssFeedOutlined/>
+                </ListItemIcon>
+                <span> Feeder </span>
+              </ListItemButton>
+
+              <ListItemButton sx={{width: '100%', height: 40}}>
+                <ListItemIcon>
+                  <Sync/>
+                </ListItemIcon>
+                <span> Syncer </span>
+              </ListItemButton>
+
+              <ListItemButton sx={{width: '100%', height: 40}}>
+                <ListItemIcon>
+                  <DataUsageTwoTone/>
+                </ListItemIcon>
+                <span> Data </span>
+              </ListItemButton>
+
+
+            </List>
+            <Button sx={{width: 'unset'}}/>
+
+          </Box>
+
+        </Drawer>
+        <AppBar position="relative">
+
+
+          <Toolbar variant="dense">
+            <IconButton onClick={()=>{setDrawerOpen(!drawerOpen)}} >
+              <GridMenuIcon style={{color:'white'}} />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+
   );
 }
 
