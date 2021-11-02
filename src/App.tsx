2b95f3499cf57ce064f763a0561dd39871f464e2
hossyn assghary <hossynassghary@gmail.com>
2021-11-02 21:41:31 +0330

    Test3

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,82 @@ function App() {
     return (
         <div className="App">
 
+            <Grid width={'auto'} border={2} borderColor={'black'}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            onClick={() => {
+                                setShowDrawer(!showDrawer)
+                            }}
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
 
+
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            {headerName}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+            </Grid>
+
+            <div>
+                <Drawer anchor={'left'} onClick={() => {
+                    setShowDrawer(!showDrawer)
+                }} open={showDrawer}>
+                    <Box style={{width: 200}}>
+                        <List>
+                            <ListItemButton style={{width: 200, height: 50}} onClick={() => {
+                                setContent(<PDashboard/>)
+                                setHeaderName('Dashboard')
+                            }}>
+                                <Dashboard/>
+                                <span>Dashboard</span>
+                            </ListItemButton>
+
+                            <ListItemButton style={{width: 200, height: 50}} onClick={() => {
+                                setContent(<PRss/>)
+                                setHeaderName('RSSs')
+                            }}>
+                                <RssFeed/>
+                                <span>RSS</span>
+                            </ListItemButton>
+                            <ListItemButton style={{width: 200, height: 50}} onClick={() => {
+                                setContent(<Pdata/>)
+                                setHeaderName('Datas')
+                            }}>
+                                <Storage/>
+                                <span>Data</span>
+                            </ListItemButton>
+                            <ListItemButton style={{width: 200, height: 50}} onClick={() => {
+                                setContent(<PNews/>);
+                                setHeaderName('News')
+                            }}>
+                                <MenuBookIcon/>
+                                <span>News</span>
+                            </ListItemButton>
+                        </List>
+
+                    </Box>
+
+                </Drawer>
+
+            </div>
+
+
+            <Container style={{width: 'auto', padding: 20, height: 'auto', background: 'lightgray'}}>
+
+                <Grid width={'auto'} border={2} borderColor={'black'} height={'auto'}>
+                    {content}
+                </Grid>
+
+            </Container>
         </div>
 
     );
